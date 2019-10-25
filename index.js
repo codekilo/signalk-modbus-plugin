46986b7c743e379165a17ddfe344f276dbf20474
Albert Smit <albertsmit93@gmail.com>
2019-10-25 11:53:34 +0200

    ran formatter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PLUGIN_ID = 'signalk-modbus-plugin';
 const PLUGIN_NAME = 'SignalK Modbus plugin';
-module.exports = function (app) {
+module.exports = function(app) {
   var plugin = {};
   var ModbusRTU = require("modbus-serial");
   var client = new ModbusRTU();
@@ -13,61 +13,63 @@ module.exports = function (app) {
   /**
    * Send a single update to SignalK.
    */
-  function handleData(data,mapping,slaveID) {
+  function handleData(data, mapping, slaveID) {
     app.debug(data);
     delta = {
       values: [{
-      path: mapping.path,
-      value: data.data[0]
-    }],
+        path: mapping.path,
+        value: data.data[0]
+      }],
       context: app.getSelfPath('uuid'),
-      $source:"modbus-tcp." + slaveID + "." + mapping.register + "." + mapping.operation,
+      $source: "modbus-tcp." + slaveID + "." + mapping.register + "." + mapping.operation,
       timestamp: new Date().toISOString()
-   };
-
-   deltas = {
-    updates: [delta]
     };
-    app.handleMessage(PLUGIN_ID,deltas);
-   }
 
-   /**
-    * Ask the server for the contents of a single register.
-    * calls handleData to send the data to SignalK
-    */
-  function pollModbus(client, mapping,slaveID) {
+    deltas = {
+      updates: [delta]
+    };
+    app.handleMessage(PLUGIN_ID, deltas);
+  }
+
+  /**
+   * Ask the server for the contents of a single register.
+   * calls handleData to send the data to SignalK
+   */
+  function pollModbus(client, mapping, slaveID) {
     client.setID(slaveID);
     switch (String(mapping.operation)) {
-      case 'fc1': 
+      case 'fc1':
         client.readCoils(mapping.register, 1)
-        .then(data => handleData(data,mapping,slaveID));
+          .then(data => handleData(data, mapping, slaveID));
         break;
-        case 'fc2':
+      case 'fc2':
         client.readDiscreteInputs(mapping.register, 1)
-        .then(data => handleData(data,mapping,slaveID));
+          .then(data => handleData(data, mapping, slaveID));
         break;
       case 'fc3':
         client.readHoldingRegisters(mapping.register, 1)
-        .then(data => handleData(data,mapping,slaveID));
+          .then(data => handleData(data, mapping, slaveID));
         break;
       case 'fc4':
-      client.readInputRegisters(mapping.register, 1)
-      .then(data => handleData(data,mapping,slaveID));
+        client.readInputRegisters(mapping.register, 1)
+          .then(data => handleData(data, mapping, slaveID));
     }
   }
 
-  plugin.start = function (options, restartPlugin) {
+  plugin.start = function(options, restartPlugin) {
     // Here we put our plugin logic
     plugin.options = options;
     app.debug('Plugin started');
     // connect to modbus server
-    client.connectTCP(options.connection.ip, { port: options.connection.port });
+    client.connectTCP(options.connection.ip, {
+      port: options.connection.port
+    });
     // setup timer to poll modbus server
-    options.slaves.forEach(slave => slave.mappings.forEach(mapping =>timers.push(setInterval(pollModbus, options.pollingInterval*1000, client, mapping, slave.slaveID))));
+    options.slaves.forEach(slave => slave.mappings.forEach(mapping => timers.push(setInterval(pollModbus, options.pollingInterval * 1000, client, mapping, slave.slaveID))));
 
   };
 
-  plugin.stop = function () {
+  plugin.stop = function() {
     // Here we put logic we need when the plugin stops
     app.debug('Plugin stopped');
     timers.forEach(timer => clearInterval(timer));
@@ -130,24 +132,23 @@ module.exports = function (app) {
                       'read input register (FC4)'
                     ],
                     default: 'fc3'
-                    },
-                    register: {
-                      type: 'number',
-                      title: 'register',
-                      default: 11
-                    },
-                    path: {
-                      type: 'string',
-                      title: "Path to store data",
-                      default: "modbus.test"
-                    }
+                  },
+                  register: {
+                    type: 'number',
+                    title: 'register',
+                    default: 11
+                  },
+                  path: {
+                    type: 'string',
+                    title: "Path to store data",
+                    default: "modbus.test"
+                  }
                 }
               }
             }
           }
         }
       }
-      
     }
   };
 
